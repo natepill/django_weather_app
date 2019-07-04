457ccd7d4023798168edee821e068f6923b3096d
Nathan Pillai <natepill@gmail.com>
2019-07-04 03:26:08 -0700

    [added and edit] forms and debugged weather views

--- weather_app/weather/views.py
+++ weather_app/weather/views.py
@@ -1,18 +1,14 @@
 from django.shortcuts import render
+from .models import City
 import requests
+
 # Create your views here.
 def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&APPID=579ada901351c01b78b7df86d0346581'
     city = 'san francisco'
 
-    city_weather = {
-        'city' : city,
-        'temperature' : weather_request['main']['temp'],
-        'description' : weather_request['weather'][0]['description'],
-        'icon' : weather_request['weather'][0]['icon'],
-    }
-
     # print(request.text)
+    cities = City.objects.all()
 
     weather_data = []
 
@@ -22,11 +18,11 @@ def index(request):
 
         city_weather = {
             'city' : city.name,
-            'temperature' : r['main']['temp'],
-            'description' : r['weather'][0]['description'],
-            'icon' : r['weather'][0]['icon'],
+            'temperature' : weather_request['main']['temp'],
+            'description' : weather_request['weather'][0]['description'],
+            'icon' : weather_request['weather'][0]['icon'],
         }
-        
+
         weather_data.append(city_weather)
 
     context = {'city_weather': city_weather}

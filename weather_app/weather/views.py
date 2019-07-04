f782a5721c2868e7381e1e1c1155ea2e50d363db
Nathan Pillai <natepill@gmail.com>
2019-07-04 02:19:59 -0700

    [added] city weather object

--- weather_app/weather/views.py
+++ weather_app/weather/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
-
+import requests
 # Create your views here.
+def index(request):
+    url = 'http://api.openweathermap.org/data/2.5/weather?q={}&APPID=579ada901351c01b78b7df86d0346581'
+    city = 'san francisco'
+
+    weather_request = requests.get(url.format(city)).json
+
+    city_weather = {
+        'city' : city.name,
+        'temperature' : r['main']['temp'],
+        'description' : r['weather'][0]['description'],
+        'icon' : r['weather'][0]['icon'],
+    }
+
+    print(weather_request.text)
+    return render(request, 'weather/weather.html')

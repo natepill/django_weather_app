0ed9cd177e0664c1a32cd96ac976cf9d0cf8d21c
Nathan Pillai <natepill@gmail.com>
2019-07-04 02:25:58 -0700

    [added] debugging views

--- weather_app/weather/views.py
+++ weather_app/weather/views.py
@@ -5,14 +5,16 @@ def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&APPID=579ada901351c01b78b7df86d0346581'
     city = 'san francisco'
 
-    weather_request = requests.get(url.format(city)).json
+    weather_request = requests.get(url.format(city)).json()
 
     city_weather = {
-        'city' : city.name,
-        'temperature' : r['main']['temp'],
-        'description' : r['weather'][0]['description'],
-        'icon' : r['weather'][0]['icon'],
+        'city' : city,
+        'temperature' : weather_request['main']['temp'],
+        'description' : weather_request['weather'][0]['description'],
+        'icon' : weather_request['weather'][0]['icon'],
     }
 
-    print(weather_request.text)
-    return render(request, 'weather/weather.html')
+    # print(request.text)
+
+    context = {'city_weather': city_weather}
+    return render(request, 'weather/weather.html', context)

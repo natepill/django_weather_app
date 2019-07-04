65850e7cd751d82ec8b67d17145edeefd6219e63
Nathan Pillai <natepill@gmail.com>
2019-07-04 03:20:44 -0700

    [added] multiple cities

--- weather_app/weather/views.py
+++ weather_app/weather/views.py
@@ -5,8 +5,6 @@ def index(request):
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&APPID=579ada901351c01b78b7df86d0346581'
     city = 'san francisco'
 
-    weather_request = requests.get(url.format(city)).json()
-
     city_weather = {
         'city' : city,
         'temperature' : weather_request['main']['temp'],
@@ -15,6 +13,21 @@ def index(request):
     }
 
     # print(request.text)
+
+    weather_data = []
+
+    for city in cities:
+
+        weather_request = requests.get(url.format(city)).json()
+
+        city_weather = {
+            'city' : city.name,
+            'temperature' : r['main']['temp'],
+            'description' : r['weather'][0]['description'],
+            'icon' : r['weather'][0]['icon'],
+        }
+        
+        weather_data.append(city_weather)
 
     context = {'city_weather': city_weather}
     return render(request, 'weather/weather.html', context)
